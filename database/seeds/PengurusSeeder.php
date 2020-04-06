cbc5ffc5c664957523992337fd48a7a0e039a93a
andip114 <andinp0907@gmail.com>
2020-04-06 08:46:25 +0700

    seeder pengurus

---
+++ database/seeds/PengurusSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Pengurus;
+
+class PengurusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //insert data pengurus
+        $faker = Faker::create('id_ID');
+ 
+    	for($i = 1; $i <= 100; $i++){
+ 
+    	      // insert data ke table pegawai menggunakan Faker
+    		DB::table('pengurus')->insert([
+                'nip' => $faker->numberBetween(100000,1200000),
+    			'nama' => $faker->name,
+                'jabatan' => $faker->name
+                ]);
+    	}
+    }
+}
